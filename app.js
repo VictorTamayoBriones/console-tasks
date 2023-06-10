2acd9ea942971fdbd1c9385efab7106fc2aa5b61
VictorTamayoBriones <victortamayo509@gmail.com>
2023-06-10 01:34:16 -0600

    feat: :sparkles: create and load data

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
 require('colors');
 
 const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
+const { saveDB, readDb } = require('./helpers/saveFile');
 const Tasks = require('./models/tasks');
 
 console.clear()
 
-const main = async ()=>{
-    
+const main = async () => {
+
     let opt = '';
     const tasks = new Tasks();
 
+    const tareasDb = readDb();
+
+    if (tareasDb) {
+        tasks.loadArrData(tareasDb);
+    }
+
     do {
-        opt = await inquirerMenu();
-        
+        opt = await inquirerMenu(); // imprime el menu
+
         switch (opt) {
             case '1':
                 // Create option
                 const desc = await readInput('Task Description:');
                 tasks.createTask(desc);
-            break;
+                break;
 
             case '2':
                 console.log(tasks._list);
-            break;
+                console.log(tasks.listArr);
+                break;
         }
+
+        // saveDB(tasks.listArr);
 
         await pause();
         // opt !== '0' && await pausa();
-    }while(opt !== '0')
-    
+    } while (opt !== '0')
+
 }
 
 main()
